c166d977f00668d9e30f07e69b0caef35fbc1d8c
saul <dlathf3210@naver.com>
2023-10-16 11:43:45 +0900

    [START]

---
+++ src/main/java/com/example/springelasticsearch_pr/SpringElasticSearchPrApplication.java
@@ -0,0 +1,13 @@
+package com.example.springelasticsearch_pr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElasticSearchPrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringElasticSearchPrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springelasticsearch_pr/SpringElasticSearchPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springelasticsearch_pr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringElasticSearchPrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
